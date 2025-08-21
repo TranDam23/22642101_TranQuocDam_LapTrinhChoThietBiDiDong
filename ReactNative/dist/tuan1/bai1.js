3eeb9e4f6d228ead15201467ae55a469e11e3391
TranDam23 <166279185+TranDam23@users.noreply.github.com>
2025-08-21 08:13:49 +0700

    commit new

---
+++ ReactNative/dist/tuan1/bai1.js
@@ -0,0 +1,12 @@
+"use strict";
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    displayInfo() {
+        console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+}
+const person1 = new Person("Alice", 25);
+person1.displayInfo();
